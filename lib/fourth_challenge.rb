ecd824a667c0b49e787739f9d29e19a818138685
Garth Lilly <garff247@gmail.com>
2019-04-21 18:23:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,14 +15,14 @@ def fourth_challenge
       },
       :hero_friends => [
         {
-          :name => "Benvolio",
-          :name => "17",
-          :attitude => "worried"
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
         },
         {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-headed"
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
         }
       ]
     },
@@ -42,14 +42,14 @@ def fourth_challenge
       },
       :heroine_friends => [
         {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
         },
         {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
         }
       ]
    }
